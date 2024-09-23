4538add30a66e285884fefa3daec7c4186b5eb03
rrekaf <pewuka2005@wp.pl>
2024-09-23 12:16:04 -0700

    rename to remy (more accurate)

---
+++ remy.nix
@@ -0,0 +1,27 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    gcc
+    gdb
+    gnumake
+    cmake
+    clang-tools
+    valgrind
+    pkg-config
+    raylib
+
+    # project specific
+    emscripten
+    cmake
+    go
+    gopls
+  ];
+
+  shellHook = ''
+    export PS1='\[\e[38;5;160;1m\]<raylibing>\[\e[0m\]:\[\e[96;3m\]\u\[\e[0m\]@\w\[\e[92m\]\$\[\e[0m\] '
+    echo "C development environment loaded"
+    echo "Available tools: gcc, gdb, make, cmake, clang-tools, valgrind"
+    gcc --version
+  '';
+}
